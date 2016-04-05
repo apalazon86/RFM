4caa64d897ee431a1ea00aa0d34141b542b7009c
Alfonso Palazón <alfonsopalazon86@hotmail.com>
2016-04-06 00:13:32 +0200

    Offers added to product data

--- public/js/main.js
+++ public/js/main.js
@@ -25,14 +25,31 @@ RFM.config(function($routeProvider){
 //Controlador de productos
 RFM.controller('ProductsController',['$scope', '$http', function($scope, $http){
   $scope.productsList=[];
+  //Obtenemos el listado de ofertas
+  dpd.offers.get(function(results, error) {
+    $scope.offersList=results;
+  });
   //Obtenemos el listado de productos
   $http.get('/products').success(function(data){
-    //Para cada producto inicializamos el campo dateAlert a true si caduca en 2 días o menos
+    //Para cada producto 
     data.forEach(function(currentprod){
+      //Inicializamos el campo dateAlert a true si caduca en 2 días o menos
       currentprod.dateAlert=dateDiffInDays(currentprod.dateOfExpiry)<=2?true:false;
-      currentprod.quantityText=formatQuantity(currentprod.quantity,currentprod.unit)
-    });
-    //console.log(data);
+      //Formateamos el texto de la cantidad de producto
+      currentprod.quantityText=formatQuantity(currentprod.quantity,currentprod.unit);
+      //Obtenemos las ofertas del producto actual
+      currentprod.offers=$scope.offersList.filter(function(el){
+        return el.foodId==currentprod.foodId;
+      }); 
+      //Si hay ofertas para el producto actual
+      if((currentprod.offers).length>0){
+        //Nos quedamos con la primera oferta
+        currentprod.offerUrl=currentprod.offers[0].url;
+      }
+      else{
+        currentprod.offerUrl="";
+      }
+    })
     $scope.productsList=data;
   });
   //Inicializamos el índice en la tabla de productos del producto seleccionado
@@ -53,16 +70,8 @@ RFM.controller('OffersController',['$scope', '$http', function($scope, $http){
   $scope.offersList=[];
   //Obtenemos el listado de ofertas
   $http.get('/offers').success(function(data){
-    //Para cada producto inicializamos el campo dateAlert a true si caduca en 2 días o menos
-    /*data.forEach(function(currentprod){
-      currentprod.dateAlert=dateDiffInDays(currentprod.dateOfExpiry)<=2?true:false;
-      currentprod.quantityText=formatQuantity(currentprod.quantity,currentprod.unit)
-    });*/
-    console.log(data);
     $scope.offersList=data;
   });        
 }]);
 
 })();
-
-    
