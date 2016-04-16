c71fa1fdcbd5b168beec353b91766d5e8a67408d
Alfonso Palazón <alfonsopalazon86@hotmail.com>
2016-04-16 19:01:37 +0200

    Recipes, products and offers interface improved

--- resources/offers/get.js
+++ resources/offers/get.js
@@ -1,2 +1,5 @@
 //Si el usuario no está autentificado no se devuelven las ofertas
 cancelUnless(me, "You are not logged in", 401);
+
+//Se añade una propiedad con el texto del precio
+this.offerText=this.prize+"€/"+this.unit;
